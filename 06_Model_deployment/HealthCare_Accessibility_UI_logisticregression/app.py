62a0e7756e03895c41574e693d293976628bb8e5
Chris <166769576+gisthuband@users.noreply.github.com>
2025-02-24 08:46:34 -0800

    Update app.py

--- 06_Model_deployment/HealthCare_Accessibility_UI_logisticregression/app.py
+++ 06_Model_deployment/HealthCare_Accessibility_UI_logisticregression/app.py
@@ -5,8 +5,52 @@ import pandas as pd
 from geopy.distance import geodesic
 import pandas as pd
 import plotly.express as px
+from datetime import date, timedelta
+from prophet import Prophet
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 import os
+
+#load the ndwi and ndvi data from the folder
+def get_data():
+    """ Get the NDVI AND NDWI VALUE INFORMATION AND INPUT IT INTO THE MODEL
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'ndvi_ndwi_values_2000_2023_gen.csv'
+    df = pd.read_csv(DATA_FILENAME)
+
+
+    df = df.drop(columns= 'Unnamed: 0')
+
+    one_hots = pd.get_dummies(df['GEO_NAME_SHORT'])
+
+    final_df = pd.concat([df, one_hots], axis=1)
+
+    return final_df
+
+#create the dataframe from the csv file
+df = get_data()
+
+#train the model on the data with the hyperparameter tuned model specifications
+def model(df):
+
+    X = df[['ndvi_value','ndwi_value','month_x','Chad','Ethiopia','South Sudan','Sudan']]
+    y = df['outbreak']
+
+    classifier = LogisticRegression(solver='liblinear', max_iter=100, C=1, class_weight='balanced')
+
+    res = classifier.fit(X , y)
+
+    return res
+
+model = model(df)
+
+
 
 print("Current Working Directory:", os.getcwd())
 
@@ -47,16 +91,98 @@ def find_nearest_hospital(user_lat, user_lon, hospital_data):
         print(f"An error occurred: {e}")
         return None
 
+#function for determining the ndvi and ndwi values based on a forecaster function
+
+def forecaster(month, year, df):
+
+    date = f'{int(year)}-{int(month)}-15'
+
+    #select the date as 'ds' and values as 'y' for the prophet model to work 
+    ndvi_df = df[['ds','ndvi_value']]
+    ndvi_df = ndvi_df.rename(columns={'ndvi_value':'y'})
+
+    ndwi_df = df[['ds','ndwi_value']]
+    ndwi_df = ndwi_df.rename(columns={'ndwi_value':'y'})
+    
+    #create an instance of a prophet forecaster
+    ndvi_fore = Prophet()
+
+    ndvi_fore.fit(ndvi_df)
+
+    ndvi_pred = ndvi_fore.predict(pd.DataFrame(data={'ds': date}, index=[0]))
+
+    ndvi_pred.head()
+
+    ndvi_val = ndvi_pred.loc[0, 'yhat']
+    
+    #now create an instance for the ndwi value
+
+    ndwi_fore = Prophet()
+
+    ndwi_fore.fit(ndwi_df)
+
+    ndwi_pred = ndwi_fore.predict(pd.DataFrame(data={'ds': date}, index=[0]))
+
+    ndwi_val = ndwi_pred.loc[0, 'yhat']
+
+
+    return ndvi_val, ndwi_val
+
+
 
 # Streamlit UI
 st.title("HealthCare Accessibility Determination System")
-st.write("By input your location, determine the possible accessibility to the nearest healthcare facility")
+st.write("By input your location and the time of year, determine the probability of a malaria outbreak and possible accessibility to the nearest healthcare facility")
+
+
 
 # user input
+#month and year for malaria probability
+
+month = st.number_input('please provide the month of the year you are looking for (1-12)')
+year = st.number_input('please provide the year you are looking for')
+
+#lat, long, and pop for healthcare accesibility model
+
 user_lat = st.number_input("Input your latitude", value=15.45, format="%.6f")
 user_lon = st.number_input("Input your longitude", value=32.49, format="%.6f")
 pop2024 = st.number_input("Input the population of your nearest city", value=1000000)
 
+
+
+###malaria outbreak probability
+inputs = None
+
+if month and year:
+    #generate ndvi and ndwi values based on time of year
+    forecast = forecaster(month, year, df)
+    #prepare the inputs for the model
+    inputs = np.array([forecast[0], forecast[1], int(month), 0 ,0 ,0 ,1])
+    
+    inputs = inputs.reshape(1,-1)
+    #predicte the probability of malaria outbreak
+    pred = model.predict(inputs)
+    pred_proba = model.predict_proba(inputs)
+
+    #verbally declare the prediction value's interpretation
+    #verbally declare the probablity of the prediction
+    
+    if pred == 0:
+        
+        st.write('the prediction is that there will not be a malaria outbreak')
+
+    elif pred == 1:
+        
+        st.write('the prediction is that there will be a malaria outbreak')
+
+
+    if pred_proba is not None:
+
+        st.write(f'the probability of a malaria outbreak occuring is {pred_proba[0,1]}')
+
+
+
+###healthcare accesibility 
 if st.button("Determine"):
     # search the nearest hospitals
 
