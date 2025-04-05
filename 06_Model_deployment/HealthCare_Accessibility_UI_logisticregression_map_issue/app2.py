6dc3252b51d8a79b7bc09f6ecd559771b5041116
Madoka Fujii <madokafuji@yahoo.com>
2025-04-05 16:05:39 -0400

    added Omdena logo image, updated app2.py, and requirements.txt

--- 06_Model_deployment/HealthCare_Accessibility_UI_logisticregression_map_issue/app2.py
+++ 06_Model_deployment/HealthCare_Accessibility_UI_logisticregression_map_issue/app2.py
@@ -11,9 +11,18 @@ from streamlit_folium import st_folium
 from datetime import date, timedelta
 from prophet import Prophet
 from sklearn.linear_model import LogisticRegression
+#from PIL import Image
 
+st.cache_data.clear()
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(current_dir, "assets", "omdena_logo.png")
+
+# image_path = os.path.join(os.path.dirname(__file__), "omdena_logo.png")
+# image = Image.open(image_path)
+
+st.image(logo_path, width=200)
+
 
 #malaria
 #load the ndwi and ndvi data from the folder
